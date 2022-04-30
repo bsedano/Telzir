374ef3702da3c208cb73a7db7259ed4fe404ecd2
bsedano <viniciusbreno11@gmail.com>
2022-04-30 10:45:54 -0300

    feat: :sparkles: Initial developing of services page
    just started messing around on services page

--- src/pages/services/index.jsx
+++ src/pages/services/index.jsx
@@ -1,7 +1,22 @@
-import { VStack } from "@chakra-ui/react";
+import {
+  VStack,
+  Select,
+  Heading,
+  Text,
+  Button,
+  HStack,
+} from "@chakra-ui/react";
+
 import NavBar from "../../components/NavBar";
 import { motion } from "framer-motion";
+import { useState } from "react";
 const Services = () => {
+  const [from, setFrom] = useState(0);
+  const [where, setWhere] = useState(0);
+  const [priceMinute, setPriceMinute] = useState(0);
+  const [promoPrice, setPromoPrice] = useState(0);
+  const [time, setTime] = useState(0);
+  console.log(where);
   return (
     <motion.div
       initial={{ opacity: 0.2 }}
@@ -11,7 +26,49 @@ const Services = () => {
     >
       <VStack spacing={0} w={"100vw"} h={"100vh"}>
         <NavBar />
-        <VStack h={"2000px"}></VStack>
+
+        <VStack border={"2px solid black"} w={"100%"} p={4}>
+          <Heading fontFamily={"Roboto"}>
+            Já conhece o serviço Fale mais?
+          </Heading>
+          <Text fontFamily={"Roboto"}>
+            Nosso mais novo serviço para chamadas de longa distância nacional,
+            com ele você fala mais pagando menos.
+          </Text>
+          <Text>Faça uma comparação de preços abaixo.</Text>
+        </VStack>
+        <VStack>
+          <Heading>Veja as vantagens de ser um cliente Fale mais.</Heading>
+          <HStack>
+            <VStack>
+              <Heading>De</Heading>
+              <Select
+                onChange={() => (e) => console.log(e)}
+                borderRadius={"0px"}
+                w={"150px"}
+              >
+                <option>11</option>
+                <option>16</option>
+                <option>17</option>
+                <option>18</option>
+              </Select>
+            </VStack>
+            <VStack>
+              <Heading>Para</Heading>
+              <Select
+                onChange={() => (e) => console.log(e)}
+                borderRadius={"0px"}
+                w={"150px"}
+              >
+                <option>11</option>
+                <option>16</option>
+                <option>17</option>
+                <option>18</option>
+              </Select>
+            </VStack>
+            <Button>Calcular preço </Button>
+          </HStack>
+        </VStack>
       </VStack>
     </motion.div>
   );
